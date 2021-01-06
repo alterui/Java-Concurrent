646a385c1afeca8f67ae2290cd1187d8a15944cf
ALTERUI <656812771@qq.com>
2021-01-06 22:21:10 +0800

    no message

--- src/main/java/com/lr/concurrent05/SemaphoreTest.java
+++ src/main/java/com/lr/concurrent05/SemaphoreTest.java
@@ -11,15 +11,16 @@ import java.util.concurrent.Semaphore;
 public class SemaphoreTest {
     private static final int THREAD_COUNT = 30;
     private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
-    private static Semaphore s = new Semaphore(2);
+    private static Semaphore s = new Semaphore(30);
     public static void main(String[] args) {
 
         for (int i = 0; i< THREAD_COUNT; i++) {
             threadPool.execute(() -> {
                 try {
                     s.acquire();
+                    Thread.sleep(1000);
                     System.out.println("save data");
-
+                    s.release();
                 } catch (InterruptedException e) {
                 }
             });
